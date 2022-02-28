386f2d517789a3043aef68830ed8c3e27d42bcd9
Allain <itsallain@gmail.com>
2022-02-28 23:28:05 +0800

    update models and migrations

---
+++ mclog_API/Models/ActivityLogsModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mclog_API.Models
+{
+    public class ActivityLogsModel
+    {
+        public int Id { get; set; }
+
+        public string? Status { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        public DateTime ActivityDate { get; set; }
+    }
+}
---
+++ mclog_API/Models/UserHealthStatusModel.cs
@@ -0,0 +1,11 @@
+﻿namespace mclog_API.Models
+{
+    public class UserHealthStatusModel
+    {
+        public int Id { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
